68cc61fac349961abec0cf3ae048e0d31bc5671b
RemediosM <puszka004@gmail.com>
2019-05-15 22:37:23 +0200

    7.1 Poem Beautifier

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,5 +1,30 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
 public class StreamMain {
+    public static void main(String[] args){
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("Lorem ipsum dolor sit amet, consectetur adipiscing elit.",(text -> "ABC " + text + " ABC"));
+        poemBeautifier.beautify("Lorem ipsum dolor sit amet, consectetur adipiscing elit.",(text -> text.toUpperCase()));
+        poemBeautifier.beautify("Lorem ipsum dolor sit amet, consectetur adipiscing elit.",(text -> text.replace('o','a')));
+        poemBeautifier.beautify("Lorem ipsum dolor sit amet, consectetur adipiscing elit.",(text -> "\"" + text.toLowerCase().concat("\"")));
+    }
 
 }
