84f5a7693352c676652c00c4cdb9b19f80338321
RemediosM <puszka004@gmail.com>
2019-05-27 21:42:24 +0200

    Module 8.4 v2

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -1,6 +1,7 @@
 package com.kodilla.exception.test;
 
 import javax.management.relation.RoleInfoNotFoundException;
+import javax.swing.text.StyledEditorKit;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,10 +13,17 @@ public class FlightFinder {
         flightsMap.put("LKO", true);
         flightsMap.put("LAN", false);
 
-        if (flightsMap.containsKey(flight.getArrivalAirport().equals(true))) {
-            System.out.println("You can fly to this airport");
+
+        if(flightsMap.containsKey(flight.getArrivalAirport())){
+            if (flightsMap.get(flight.getArrivalAirport())){
+                System.out.println("You can fly to this airport");
+            } else {
+                System.out.println("You can't fly to this airport");
+            }
         } else {
-            throw new RouteNotFoundException("You can't fly to this airport");
+            throw new RouteNotFoundException("There's no such airport");
         }
+
     }
 }
+
--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinderRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinderRunner.java
@@ -2,7 +2,7 @@ package com.kodilla.exception.test;
 
 public class FlightFinderRunner {
     public static void main(String[] args) {
-        Flight flight = new Flight("SIN", "YYU");
+        Flight flight = new Flight("SIN", "WAW");
 
         FlightFinder flightFinder = new FlightFinder();
 
