f874e3085ccb505c4bce5d23ece53bf000336041
RemediosM <puszka004@gmail.com>
2019-07-21 20:14:15 +0200

    Module 13.1 good patterns

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Main.java
@@ -0,0 +1,17 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+
+        String moviesTitles = movieStore.getMovies().entrySet().stream()
+                .flatMap(movie -> movie.getValue().stream())
+                .collect(Collectors.joining("!", "",""));
+
+        System.out.println(moviesTitles);
+    }
+}
