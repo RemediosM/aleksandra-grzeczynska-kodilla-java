c02f9ab85212daefc0e29f75b0b1ef6e78065407
RemediosM <puszka004@gmail.com>
2019-07-31 21:59:32 +0200

    15.5 Builder

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/Bigmac.java
@@ -0,0 +1,63 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Bigmac {
+    private final String bun;
+    private final int burgers;
+    private final List<String> ingredients;
+
+    public static class BigmacBuilder {
+        private String bun;
+        private int burgers;
+        private List<String> ingredients = new ArrayList<>();
+
+        public BigmacBuilder bun(String bun) {
+            this.bun = bun;
+            return this;
+        }
+
+        public BigmacBuilder burgers(int burgers) {
+            this.burgers = burgers;
+            return this;
+        }
+
+        public BigmacBuilder ingredient(String ingredient) {
+            ingredients.add(ingredient);
+            return this;
+        }
+
+        public Bigmac build() {
+            return new Bigmac(bun, burgers, ingredients);
+        }
+    }
+
+    private Bigmac(final String bun, final int burgers, List<String> ingredients) {
+        this.bun = bun;
+        this.burgers = burgers;
+        this.ingredients = new ArrayList<>(ingredients);
+    }
+
+    public String getBun() {
+        return bun;
+    }
+
+    public int getBurgers() {
+        return burgers;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    @Override
+    public String toString() {
+        return "Bigmac{" +
+                "bun='" + bun + '\'' +
+                ", burgers=" + burgers +
+                ", ingredients=" + ingredients +
+                '}';
+    }
+
+}
